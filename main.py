736ddfe69c07f4b392650f827f3fd91dc9870950
RioluM <damian04121@gmail.com>
2022-04-06 19:26:54 +0200

    Prepared main file for user

--- main.py
+++ main.py
@@ -1,5 +1,22 @@
+from dominoes_algorithm import get_iteration_result
+from dominoes_reversed_algorithm import get_reversed_iteration_result
 
-
-domino_string = input()
-
-print(domino_string)
+print("Choose algorithm:")
+print("1 - Dominoes algorithm")
+print("2 - Reversed dominoes algorithm")
+try:
+    algorithm = int(input())
+    print("Type number of iterations")
+    iterations = int(input())
+except ValueError as e:
+    print("Wrong input")
+else:
+    print("Type dominoes")
+    domino_string = input()
+    dominoes = []
+    if algorithm == 1:
+        print(get_iteration_result(domino_string, iterations))
+    elif algorithm == 2:
+        print(get_reversed_iteration_result(domino_string, iterations))
+    else:
+        print("Wrong option")
